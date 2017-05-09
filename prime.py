cf543ed9e45b5d15219ec0366401d3f711e0acde
Anthony Karuri <anthonykaruri2010@gmail.com>
2017-05-09 22:29:23 +0300

    Test if a Prime Number is Prime(True)

---
+++ prime.py
@@ -0,0 +1,21 @@
+def is_prime(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  if type(n) == str: return False
+  r = int(n**0.5)
+  f = 5
+  while f <= r:
+    print '\t',f
+    if n%f == 0: return False
+    if n%(f+2) == 0: return False
+    f +=6
+  return True 
+
+import unittest
+
+class Prime_test(unittest.TestCase):
+  def test_prime(self):
+    self.assertEqual(is_prime(1), True)
+unittest.main()
