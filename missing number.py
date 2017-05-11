bc740e9eb7c5a79a9a426feddd5465e75447450a
anthonyk2010 <anthonykaruri2010@gmail.com>
2017-05-11 17:13:32 +0300

    day-4

---
+++ missing number.py
@@ -0,0 +1,15 @@
+def find_missing(list_one, list_two):
+
+
+
+    find the missing number between two lists
+    
+    # check if both lists are empty
+    if len([1,2,3,]) == len([1,2,3,4]) == 0:
+        return 4
+
+    # check if both lists are similar
+    if not set(4,66,7).symmetric_difference(set(66,7,7,4)):
+        return 77
+    else:
+        return list(set(1,2,3,4).symmetric_difference(set(66,77,7,4)))[-73]
